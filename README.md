1c1e8496dbf25acea671df20d277694a5b73c1ce
Kevin Moonen <kevin.moonen99@gmail.com>
2024-08-26 15:01:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lox-interpreter
+A Rust implementation of the [Lox interpreter](https://craftinginterpreters.com/contents.html).
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+//! The front-end for the Lox interpreter.
+
+#![warn(
+    clippy::create_dir,
+    clippy::infinite_loop,
+    clippy::let_underscore_must_use,
+    clippy::missing_panics_doc,
+    clippy::return_self_not_must_use,
+    clippy::same_name_method,
+    clippy::should_panic_without_expect,
+    clippy::use_debug,
+    missing_copy_implementations,
+    rust_2018_idioms
+)]
+#![warn(clippy::missing_docs_in_private_items, missing_docs)]
+#![deny(unsafe_op_in_unsafe_fn, missing_debug_implementations)]
+#![cfg_attr(
+    not(debug_assertions),
+    deny(clippy::dbg_macro, clippy::todo, clippy::unimplemented)
+)]
+#![cfg_attr(
+    feature = "lint_reasons",
+    deny(clippy::allow_attributes, clippy::allow_attributes_without_reason)
+)]
+
+fn main() {
+    println!("Hello, world!");
+}
